19ec9ad8bd38d3a768d3058cf96dea82e8c091a1
Guilherme <guilhermesergiooliveira55@gmail.com>
2020-07-09 00:30:53 -0300

    evolution chain created

--- frontend/src/components/template/Evolutions/index.jsx
+++ frontend/src/components/template/Evolutions/index.jsx
@@ -10,14 +10,25 @@ const EvolutionChain = ({
 }) => {
 
     const [pokeSpecies, setPokeSpecies] = useState({});
+    const [evolutionChain, setEvolutionChain] = useState([]);
 
-    function getEvolutionChain(data) {
-        const nextEvolve = data.evolves_to;
-        const evolutionChain = [nextEvolve.species.name];
-
-        while(nextEvolve.length !== 0){
-            getEvolutionChain(nextEvolve);
+    function getEvolutionChain(data){
+        function getChain(data, chain, index) {
+            if(data){
+                chain.push({
+                    name: data.species.name,
+                    index
+                })
+                data.evolves_to.forEach((pokemon) => {
+                    getChain(pokemon, chain, index + 1);
+                })
+            }
+            return chain;
         }
+
+        const evolutions = getChain(data.chain, [], 0);
+        console.log(evolutions);
+        return evolutions;
     }
 
     useEffect(() => {
@@ -32,8 +43,8 @@ const EvolutionChain = ({
         if(pokeSpecies.evolution_chain){
             api.get(pokeSpecies.evolution_chain.url)
                 .then(res => {
-                    const evolutionChain = getEvolutionChain(res.data.chain);
-                    setEvolutionChain(evolutionChain);
+                    const evolutions = getEvolutionChain(res.data);
+                    setEvolutionChain(evolutions);
                 })
         }
     }, [pokeSpecies]);
